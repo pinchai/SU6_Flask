fd1d80c023cbbb13c5764a700af9e49b9397bbed
Pinchai <>
2025-07-14 08:53:32 +0700

    add detail page and vue

--- app.py
+++ app.py
@@ -14,6 +14,18 @@ def home():
     return render_template('home.html', product_list=product_list)
 
 
+@app.get("/product-detail/<int:pro_id>")
+def product_detail(pro_id):
+    product = []
+    api_url = f"https://fakestoreapi.com/products/{pro_id}"
+    r = requests.get(api_url)
+    if r.status_code == 200:
+        product = r.json()
+    print(product)
+
+    return render_template('product_detail.html', product=product)
+
+
 @app.get('/about')
 def about():
     return render_template('about.html')
